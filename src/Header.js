35b9f52b2f577d2b2f3a46398da540d971ec5989
zaidsaid-007 <64166616+zaidsaid-007@users.noreply.github.com>
2023-01-08 11:38:09 +0300

    Ummanetic

---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+function Header() {
+    return(
+        <div className='Header'>
+        <img className="Header_logo"
+            src='Umma.png'alt='umma logo' />
+        
+        
+
+        <div className='Header_search'>
+            <input className="Header_search input"
+                type="text" />
+            <SearchIcon
+            className="Header_searchIcon" />
+            
+        </div>
+ 
+        <div className='Header_nav'>
+            <div className='Header_option'>
+                {/*sign in*/}
+                <span className='header_option_line_1'>
+                    Hello user
+                </span>
+                <span className='header_option_line_2'>
+                    Sign in
+                </span>
+
+            </div>
+            
+            <div className='Header_option'>
+                {/*sign in*/}
+                <span className='header_option_line_1'>
+                    Returns 
+                </span>
+                <span className='header_option_line_2'>
+                    & Orders
+                </span>
+
+            </div>
+
+            <div className='Header_option'>
+                {/*sign in*/}
+                <span className='header_option_line_1'>
+                    Your
+                </span>
+                <span className='header_option_line_2'>
+                    Prime
+                </span>
+
+            </div>
+
+            <div className='Header_basket'>
+                <AddShoppingCartIcon />
+                <span className='header_option_line_2 header_basketCount'>0</span>
+                    
+            </div>
+
+
+         
+        </div>
+    </div>
+ 
+    );
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA6ZhB9dM0TVrrysGYR9HyIfhFUeU45FMM",
+    authDomain: "ummanetic.firebaseapp.com",
+    projectId: "ummanetic",
+    storageBucket: "ummanetic.appspot.com",
+    messagingSenderId: "650073199461",
+    appId: "1:650073199461:web:06709e431aa1204027878c",
+    measurementId: "G-BZD5E1PC8W"
+};
